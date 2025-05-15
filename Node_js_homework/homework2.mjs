8f193c0f3f96e3c269e99b76badf2aef4183f427
Lâm Anh Khôi <lamanhkhoi06@gmail.com>
2025-05-16 01:35:56 +0700

    Auto stash before checking out "origin/main"

--- Node_js_homework/homework2.mjs
+++ Node_js_homework/homework2.mjs
@@ -24,6 +24,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
     res.end(`API count: ${sumApiCallCount}`);
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
 
 >>>>>>> Stashed changes
